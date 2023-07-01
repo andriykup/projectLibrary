c2efdaddbb4ecaee9c3e0194ebe6eaa572a60a62
Andriy Kupinskyy <andriy.khk@gmail.com>
2023-07-01 21:23:32 +0200

    git test** delete this commit

--- scrypt.js
+++ scrypt.js
@@ -54,3 +54,23 @@ function displayBooks(){
     });
 }
 
+
+
+/*
+
+https://www.w3docs.com/snippets/javascript/how-to-create-a-popup-form-using-javascript.html
+
+4 - Add a “NEW BOOK” button that brings up a form allowing users to input the details 
+for the new book: author, title, number of pages, whether it’s been read and anything 
+else you might want. You will most likely encounter an issue where submitting your 
+form will not do what you expect it to do. That’s because the submit input tries to 
+send the data to a server by default. If you’ve done the bonus section for the 
+calculator assignment, you might be familiar with event.preventDefault();. 
+Read up on the event.preventDefault documentation again and see how you can solve 
+this issue!
+
+6 - Add a button on each book’s display to change its read status.
+        1 - To facilitate this you will want to create the function that toggles a 
+            book’s read status on your Book prototype instance.
+
+*/
